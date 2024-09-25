31b2aeea7c67fa94a50b4cb87ffa9833afcbf16e
Siva <sivarohit36@gmail.com>
2024-09-25 07:34:22 +0530

    old file

--- ae_indent_app/settings.py
+++ ae_indent_app/settings.py
@@ -75,7 +75,10 @@ DATABASES = {
         "USER" : "root",
         "PASSWORD" : "",
         "HOST" : "127.0.0.1",
-        "PORT" : "3306"
+        "PORT" : "3306",
+        'OPTIONS': {
+            'sql_mode': 'traditional'
+        }
     }
 }
 
