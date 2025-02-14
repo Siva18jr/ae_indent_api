a623de52aef21a24aee7868355545a928c3d85ae
Siva <sivarohit36@gmail.com>
2025-02-14 09:06:56 +0530

    send email otp issue fixed

--- ae_indent_api/settings.py
+++ ae_indent_api/settings.py
@@ -127,4 +127,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ananthenterprisesok@gmail.com'
-EMAIL_HOST_PASSWORD = 'jsbtnebuyeudztbd'
+EMAIL_HOST_PASSWORD = 'dmvwpaohuygmivur'
